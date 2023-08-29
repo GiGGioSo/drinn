65f89599d19049d897bebcf1ca9dfafea2d37243
Wol_loW <gioele.pettarini@gmail.com>
2023-08-29 17:08:41 +0200

    Now the internet adapters info gets printed. Added the option --no-adapters-info to not print the adapters info list. Removed the option --set-drinn. The 'drinn.mp3' file is searched in the same directory of the executable.

--- drinn_server/build.bat
+++ drinn_server/build.bat
@@ -3,7 +3,7 @@
 SET SRCS=drinn.c
 SET EXE=drinn.exe
 
-SET LIBS=-lWs2_32.lib
+SET LIBS=-lWs2_32.lib -lKernel32.lib -lAdvapi32.lib -lIphlpapi.lib
 SET PREPROCESSOR_DEFINITIONS=-D _CRT_SECURE_NO_WARNINGS
 
 DEL /Q bin
--- drinn_server/drinn.c
+++ drinn_server/drinn.c
@@ -1,25 +1,46 @@
 #include <winsock2.h>
+#include <iphlpapi.h>
 #include <ws2tcpip.h>
+#include <fileapi.h>
 #include <string.h>
 #include <stdio.h>
-
 #define MINIAUDIO_IMPLEMENTATION
 #include "include/miniaudio.h"
 
-#define DRINN_DEBUG 1
+/* THIS IS NOT EXACTLY WHAT'S PRINTED IN THE HELP MESSAGE
 
-#if DRINN_DEBUG
-    #define DRINN_PRINT_DEBUG(...)\
-    do {\
-        printf(__VA_ARGS__);\
-    } while (0);
-#else
-    #define DRINN_PRINT_DEBUG(...) (void)0
+drinn:
+
+    --help                      Print this message.
+
+    --set-volume <volume>       Set volume. 
+                                It has to be greater than 0 and less or equal to 1.
+                                (default 0.8)
+
+    --debug                     Activate debug output.
+
+    --set-autostart             Set the program to autostart with the same flags.
+
+*/ 
+
+#define DRINN_CURRENT_VERSION "0.2"
+
+#ifndef TRUE
+#define TRUE 1
 #endif
+#ifndef FALSE
+#define FALSE 0
+#endif
+typedef char drinn_bool;
+
+#define DRINN_PRINT_DEBUG(...)\
+do {\
+    if (debug_output) printf("[DEBUG] "__VA_ARGS__);\
+} while (0);
 
 #define DRINN_PRINT_ERROR(...)\
 do {\
-    fprintf(stderr, __VA_ARGS__);\
+    fprintf(stderr, "[ERROR] "__VA_ARGS__);\
 } while (0);
 
 #define DRINN_PRINT(...)\
@@ -39,12 +60,13 @@ typedef enum {
     DRINN_GETADDRINFO_ERROR = 7,
     DRINN_BIND_ERROR = 8,
     DRINN_CONNECT_ERROR = 9,
-    DRINN_STARTUP_ERROR = 10
+    DRINN_STARTUP_ERROR = 10,
+    DRINN_INVALID_ARGS_ERROR = 11,
+    DRINN_INSUFFICIENT_BUFFER_ERROR = 12,
+    DRINN_INET_NTOP_ERROR = 13,
+    DRINN_GET_ADAPTERS_ERROR = 14,
+    DRINN_GENERIC_ERROR = 33,
 } drinn_err;
-
-#define TRUE 1
-#define FALSE 0
-typedef char bool;
 
 #define DRINN_RECV_BUFF_LEN 512
 #define DRINN_SEND_BUFF_LEN 256
@@ -53,49 +75,191 @@ typedef char bool;
 #define DRINN_ETX '~'
 
 #define DRINN_DEFAULT_PORT "27333"
-
-#define DRINN_DEFAULT_VOLUME 1.0f
+#define DRINN_DEFAULT_VOLUME 0.8f
+#define DRINN_DEFAULT_AUDIO_FILE "drinn.mp3"
 
 #define ARR_LENGTH(arr) (sizeof((arr)) / sizeof((arr)[0]))
 
-drinn_err manage_connection(SOCKET client_socket);
+drinn_err
+manage_connection(SOCKET client_socket);
+drinn_err
+terminate_socket(SOCKET socket);
+drinn_bool
+file_exists(const char *path);
+void
+print_help_message();
 
 ma_engine drinn_sound_engine;
 ma_sound drinn_sound;
 
-int main(int argc, char *argv[]) {
+drinn_bool debug_output = FALSE;
 
+int main(int argc, char *argv[]) {
+    DRINN_PRINT("################################################\n");
+    DRINN_PRINT("############  WELCOME TO DRINN v"DRINN_CURRENT_VERSION" ############\n");
+    DRINN_PRINT("################################################\n\n");
+
+    // ### ARGUMENTS PARSING
+    float volume_arg = -1.f;
+    drinn_bool set_autostart_arg = FALSE;
+    drinn_bool print_adapters_info = TRUE;
+
+    for(int arg_index = 1; arg_index < argc; ++arg_index) {
+        if (strcmp("--help", argv[arg_index]) == 0) {
+            print_help_message();
+            return DRINN_SUCCESS;
+        } else if (strcmp("--debug", argv[arg_index]) == 0) {
+            debug_output = TRUE;
+        } else if (strcmp("--set-autostart", argv[arg_index]) == 0) {
+            set_autostart_arg = TRUE;
+        } else if (strcmp("--no-adapters-info", argv[arg_index]) == 0) {
+            print_adapters_info = FALSE;
+        } else if (volume_arg == -1.f &&
+                    strcmp("--set-volume", argv[arg_index]) == 0) {
+            if (arg_index == argc-1) {
+                DRINN_PRINT_ERROR("You must provide a number after --set-volume\n");
+                print_help_message();
+                return DRINN_INVALID_ARGS_ERROR;
+            } else {
+                char *junk;
+                volume_arg = strtof(argv[arg_index+1], &junk);
+                if (*junk != '\0' || !(0.f < volume_arg && volume_arg <= 1.f)) {
+                    DRINN_PRINT_ERROR("Invalid volume value: %s\n", argv[arg_index+1]);
+                    print_help_message();
+                    return DRINN_INVALID_ARGS_ERROR;
+                }
+                arg_index++;
+            }
+        } else {
+            DRINN_PRINT_ERROR("Unrecognised argument: %s\n", argv[arg_index]);
+            print_help_message();
+            return DRINN_INVALID_ARGS_ERROR;
+        }
+    }
+    if (volume_arg == -1.f) volume_arg = DRINN_DEFAULT_VOLUME;
+    // ### END OF ARGUMENTS PARSING
+
+    // ### SETTING CORRECT WORKING DIRECTORY
+    int work_dir_path_size = 8192;
+    char *work_dir_path = (char *) calloc(work_dir_path_size, sizeof(char));
+    int full_exe_path_size = 4096;
+    char *full_exe_path = (char *) calloc(full_exe_path_size, sizeof(char));
+    DWORD filename_result =
+        GetModuleFileNameA(NULL, full_exe_path, full_exe_path_size-1);
+    if (filename_result != 0) {
+        char drive[64] = "";
+        char dir[512] = "";
+        _splitpath_s(full_exe_path,
+                     drive, sizeof(drive),
+                     dir, sizeof(dir),
+                     NULL, 0,
+                     NULL, 0);
+        _makepath_s(work_dir_path, work_dir_path_size-1,
+                    drive, dir, NULL, NULL);
+        if (SetCurrentDirectory(work_dir_path) == 0) {
+            DRINN_PRINT_ERROR("Could not current working directory: %ld\n", GetLastError());
+            if (work_dir_path) free(work_dir_path);
+            if (full_exe_path) free(full_exe_path);
+            return DRINN_GENERIC_ERROR;
+        }
+        DRINN_PRINT_DEBUG("Successfully set current working directory: %s\n",
+                          work_dir_path);
+    } else {
+        DRINN_PRINT_ERROR("Could not get full executable path: %ld\n", GetLastError());
+        if (work_dir_path) free(work_dir_path);
+        if (full_exe_path) free(full_exe_path);
+        return DRINN_GENERIC_ERROR;
+    }
+
+    DRINN_PRINT_DEBUG("### INFO FROM ARGS ###\n");
+    DRINN_PRINT_DEBUG("working_dir: %s\n", work_dir_path);
+    DRINN_PRINT_DEBUG("debug: %s\n", (debug_output)?"true":"false");
+    DRINN_PRINT_DEBUG("volume: %f\n", volume_arg);
+    DRINN_PRINT_DEBUG("autostart: %s\n", (set_autostart_arg)?"true":"false");
+    // ### END OF SETTING CORRECT WORKING DIRECTORY
+
+    // ### SETTING AUTOSTART - REGISTRY KEY CREATION
+    if (set_autostart_arg) {
+        int full_command_size = 8192;
+        char *full_command = (char *) calloc(full_command_size, sizeof(char));
+        int concat_res =
+            snprintf(full_command, full_command_size,
+                    "%s --set-volume %f %s %s",
+                    full_exe_path, volume_arg,
+                    (debug_output) ? " --debug" : "",
+                    (print_adapters_info) ? "" : " --no-adapters-info");
+        if (concat_res > full_command_size) {
+            DRINN_PRINT_ERROR("Insufficient buffer size to hold the full autostart command\n");
+            if (full_command) free(full_command);
+            if (work_dir_path) free(work_dir_path);
+            if (full_exe_path) free(full_exe_path);
+            return DRINN_INSUFFICIENT_BUFFER_ERROR;
+        }
+        if (concat_res < 0) {
+            DRINN_PRINT_ERROR("An encoding error occured while composing the autostart command\n");
+            if (full_command) free(full_command);
+            if (work_dir_path) free(work_dir_path);
+            if (full_exe_path) free(full_exe_path);
+            return DRINN_INSUFFICIENT_BUFFER_ERROR;
+        }
+        HKEY drinn_autostart_key = NULL;
+        LSTATUS create_reg_result =
+            RegCreateKeyA(
+                HKEY_CURRENT_USER,
+                "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",
+                &drinn_autostart_key);
+        if (create_reg_result == 0) {
+            LSTATUS set_reg_result =
+                RegSetValueExA(
+                    drinn_autostart_key,
+                    "DRINN", 0, REG_SZ,
+                    (unsigned char *) full_command,
+                    strlen(full_command)+1);
+            if (set_reg_result != 0) {
+                DRINN_PRINT_ERROR("Error setting the value of the registry key to autostart drinn: %ld\n", set_reg_result);
+                if (full_command) free(full_command);
+                if (work_dir_path) free(work_dir_path);
+                if (full_exe_path) free(full_exe_path);
+                return DRINN_GENERIC_ERROR;
+            }
+        } else {
+            DRINN_PRINT_ERROR(
+                "Error creating the registry key to autostart drinn: %ld\n",
+                create_reg_result);
+            if (full_command) free(full_command);
+            if (work_dir_path) free(work_dir_path);
+            if (full_exe_path) free(full_exe_path);
+            return DRINN_GENERIC_ERROR;
+        }
+        DRINN_PRINT_DEBUG("Successfully set autostart register key with content: %s\n", full_command);
+        if (full_command) free(full_command);
+    }
+    if (work_dir_path) free(work_dir_path);
+    if (full_exe_path) free(full_exe_path);
+    // ### END OF SETTING AUTOSTART
+
+    // ### SOUND INITIALIZATION
     ma_result ma_ret;
     ma_ret = ma_engine_init(NULL, &drinn_sound_engine);
     if (ma_ret != MA_SUCCESS) {
-        DRINN_PRINT_ERROR("[ERROR] Could not initialize the audio engine\n");
+        DRINN_PRINT_ERROR("Could not initialize the audio engine\n");
         return DRINN_STARTUP_ERROR;
     }
-
+    DRINN_PRINT_DEBUG("Audio engine successfully created\n");
     ma_ret = ma_sound_init_from_file(&drinn_sound_engine,
-                                     "drinn.mp3",
+                                     DRINN_DEFAULT_AUDIO_FILE,
                                      MA_SOUND_FLAG_DECODE, NULL, NULL,
                                      &drinn_sound);
     if (ma_ret != MA_SUCCESS) {
-        DRINN_PRINT_ERROR("[ERROR] Could not initialize the drinn sound\n");
+        DRINN_PRINT_ERROR("Could not initialize the drinn sound\n");
         return DRINN_STARTUP_ERROR;
     }
+    ma_sound_set_volume(&drinn_sound, volume_arg);
+    DRINN_PRINT_DEBUG("Sound \"%s\" successfully loaded\n", DRINN_DEFAULT_AUDIO_FILE);
+    // ### END OF SOUND INITIALIZATION
 
-    float volume = DRINN_DEFAULT_VOLUME;
-    if (argc > 1) {
-        DRINN_PRINT_DEBUG("Trying to set volume of %s\n", argv[1]);
-        char *junk;
-        volume = strtof(argv[1], &junk);
-        if (*junk != '\0') {
-            volume = DRINN_DEFAULT_VOLUME;
-        }
-    }
-    DRINN_PRINT("Set volume of %f\n", volume);
-    ma_sound_set_volume(&drinn_sound, volume);
-
-
-
-    bool running = TRUE;
+    // ### WSA INITIALIZATION AND SOCKET OPENING
+    drinn_bool running = TRUE;
     int result;
 
     SOCKET listen_socket = INVALID_SOCKET;
@@ -105,10 +269,9 @@ int main(int argc, char *argv[]) {
 
     result = WSAStartup(MAKEWORD(2,2), &wsa_data);
     if (result) {
-        DRINN_PRINT_ERROR("[ERROR] WSAStartup failed: %d\n", result);
+        DRINN_PRINT_ERROR("WSAStartup failed: %d\n", result);
         return DRINN_STARTUP_ERROR;
     }
-
     DRINN_PRINT_DEBUG("WSAStartup successful!\n");
 
     struct addrinfo *result_info = NULL;
@@ -122,53 +285,110 @@ int main(int argc, char *argv[]) {
 
     result = getaddrinfo(NULL, DRINN_DEFAULT_PORT, &hints, &result_info);
     if (result) {
-        DRINN_PRINT_ERROR("[ERROR] getaddrinfo failed: %d\n", result);
+        DRINN_PRINT_ERROR("getaddrinfo failed: %d\n", result);
         WSACleanup();
         return DRINN_GETADDRINFO_ERROR;
     }
-
     DRINN_PRINT_DEBUG("Getaddrinfo successful!\n");
+
+    // ### PRINTING ADAPTER INFO
+    if (print_adapters_info) {
+        DWORD return_value, addresses_size;
+        PIP_ADAPTER_ADDRESSES adapter_addresses = NULL;
+        return_value = GetAdaptersAddresses(result_info->ai_family,
+                                            GAA_FLAG_SKIP_ANYCAST |
+                                            GAA_FLAG_SKIP_MULTICAST |
+                                            GAA_FLAG_SKIP_DNS_SERVER |
+                                            GAA_FLAG_INCLUDE_PREFIX,
+                                            NULL, NULL,
+                                            &addresses_size);
+        if (return_value != ERROR_BUFFER_OVERFLOW) {
+            DRINN_PRINT_ERROR("GetAdaptersAddresses failed: %lu\n", return_value);
+            freeaddrinfo(result_info);
+            WSACleanup();
+            return DRINN_GET_ADAPTERS_ERROR;
+        }
+        adapter_addresses = (PIP_ADAPTER_ADDRESSES) malloc(addresses_size);
+        return_value = GetAdaptersAddresses(result_info->ai_family,
+                                            GAA_FLAG_SKIP_ANYCAST |
+                                            GAA_FLAG_SKIP_MULTICAST |
+                                            GAA_FLAG_SKIP_DNS_SERVER |
+                                            GAA_FLAG_INCLUDE_PREFIX,
+                                            NULL, adapter_addresses,
+                                            &addresses_size);
+        if (return_value != ERROR_SUCCESS) {
+            DRINN_PRINT_ERROR("GetAdaptersAddresses failed: %lu\n", return_value);
+            free(adapter_addresses);
+            freeaddrinfo(result_info);
+            WSACleanup();
+            return DRINN_GET_ADAPTERS_ERROR;
+        }
+        DRINN_PRINT("###################\n");
+        DRINN_PRINT("## Adapters info ##\n");
+        DRINN_PRINT("###################\n");
+        DRINN_PRINT("#\n");
+        for(PIP_ADAPTER_ADDRESSES aa = adapter_addresses;
+            aa != NULL; aa = aa->Next) {
+            char buf[BUFSIZ];
+            memset(buf, 0, BUFSIZ);
+            WideCharToMultiByte(CP_ACP, 0,
+                                aa->FriendlyName,wcslen(aa->FriendlyName),
+                                buf, BUFSIZ, NULL, NULL);
+            DRINN_PRINT("# Adapter: %s\n", buf);
+            for(PIP_ADAPTER_UNICAST_ADDRESS ua = aa->FirstUnicastAddress;
+                ua != NULL; ua = ua->Next) {
+                memset(buf, 0, BUFSIZ);
+                getnameinfo(ua->Address.lpSockaddr,
+                            ua->Address.iSockaddrLength,
+                            buf, sizeof(buf),
+                            NULL, 0, NI_NUMERICHOST);
+                DRINN_PRINT("#   %s\n", buf);
+            }
+            DRINN_PRINT("#\n");
+        }
+        DRINN_PRINT("###################\n\n");
+        free(adapter_addresses);
+    }
+    // ### END OF PRINTING ADAPTER INFO
 
     listen_socket = socket(result_info->ai_family,
                            result_info->ai_socktype,
                            result_info->ai_protocol);
 
     if (listen_socket == INVALID_SOCKET) {
-        DRINN_PRINT_ERROR("[ERROR] socket failed: %d\n", WSAGetLastError());
+        DRINN_PRINT_ERROR("socket failed: %d\n", WSAGetLastError());
         freeaddrinfo(result_info);
         WSACleanup();
         return DRINN_SOCKET_ERROR;
     }
-
     DRINN_PRINT_DEBUG("Socket successful!\n");
 
     result = bind(listen_socket, result_info->ai_addr,
                   (int)result_info->ai_addrlen);
     freeaddrinfo(result_info); // free wether bind was successful or not
     if (result) {
-        DRINN_PRINT_ERROR("[ERROR] bind failed: %d\n", WSAGetLastError());
+        DRINN_PRINT_ERROR("bind failed: %d\n", WSAGetLastError());
         closesocket(listen_socket);
         return DRINN_BIND_ERROR;
     }
-
     DRINN_PRINT_DEBUG("Bind successful!\n");
 
     if (listen(listen_socket, SOMAXCONN) == SOCKET_ERROR) {
-        DRINN_PRINT_ERROR("[ERROR] listen failed: %d\n", WSAGetLastError());
+        DRINN_PRINT_ERROR("listen failed: %d\n", WSAGetLastError());
         closesocket(listen_socket);
         WSACleanup();
         return DRINN_LISTEN_ERROR;
     }
-
     DRINN_PRINT_DEBUG("Listen successful!\n");
+    // ### END OF WSA INITIALIZATION AND SOCKET OPENING
 
-    DRINN_PRINT("Socket successfully opened!\n");
+    // DRINN_PRINT("Server local IP: %s\n", server_ip);
 
     while (running) {
-        DRINN_PRINT("Waiting to accept a connection...\n");
+        DRINN_PRINT("Ready to DRINN...\n");
         client_socket = accept(listen_socket, NULL, NULL);
         if (client_socket == INVALID_SOCKET) {
-            DRINN_PRINT_ERROR("[ERROR] accept failed: %d\n", WSAGetLastError());
+            DRINN_PRINT_ERROR("accept failed: %d\n", WSAGetLastError());
             closesocket(listen_socket);
             WSACleanup();
             return 1;
@@ -189,7 +409,7 @@ int main(int argc, char *argv[]) {
 
 drinn_err terminate_socket(SOCKET socket) {
     if (shutdown(socket, SD_BOTH) == SOCKET_ERROR) {
-        DRINN_PRINT_ERROR("[ERROR] shutdown failed: %d\n", WSAGetLastError());
+        DRINN_PRINT_ERROR("shutdown failed: %d\n", WSAGetLastError());
         closesocket(socket);
         return DRINN_SHUTDOWN_ERROR;
     }
@@ -199,8 +419,8 @@ drinn_err terminate_socket(SOCKET socket) {
 }
 
 drinn_err manage_connection(SOCKET client_socket) {
-    bool receive_error = FALSE;
-    bool receiving = TRUE;
+    drinn_bool receive_error = FALSE;
+    drinn_bool receiving = TRUE;
 
     int send_result;
     int result;
@@ -276,7 +496,7 @@ drinn_err manage_connection(SOCKET client_socket) {
             strncpy(send_buffer, "#ERR:NO_ETX_FOUND~\0",
                     ARR_LENGTH(send_buffer));
         } else {
-            DRINN_PRINT_ERROR("[ERROR] recv failed: %d\n", WSAGetLastError());
+            DRINN_PRINT_ERROR("recv failed: %d\n", WSAGetLastError());
             closesocket(client_socket);
             return DRINN_RECV_ERROR;
         }
@@ -294,7 +514,7 @@ drinn_err manage_connection(SOCKET client_socket) {
     // Closing the receive socket, can still send
     result = shutdown(client_socket, SD_RECEIVE);
     if (result == SOCKET_ERROR) {
-        DRINN_PRINT_ERROR("[ERROR] shutdown failed: %d\n", WSAGetLastError());
+        DRINN_PRINT_ERROR("shutdown failed: %d\n", WSAGetLastError());
         closesocket(client_socket);
         WSACleanup();
         return DRINN_SHUTDOWN_ERROR;
@@ -302,7 +522,7 @@ drinn_err manage_connection(SOCKET client_socket) {
 
     send_result = send(client_socket, send_buffer, strlen(send_buffer), 0);
     if (send_result == SOCKET_ERROR) {
-        DRINN_PRINT_ERROR("[ERROR] send failed: %d\n", WSAGetLastError());
+        DRINN_PRINT_ERROR("send failed: %d\n", WSAGetLastError());
         closesocket(client_socket);
         return DRINN_SEND_ERROR;
     }
@@ -311,4 +531,22 @@ drinn_err manage_connection(SOCKET client_socket) {
 
     // Closing the connection with this client
     return terminate_socket(client_socket);
+}
+
+drinn_bool file_exists(const char *path) {
+    DWORD dwAttrib = GetFileAttributes(path);
+
+    return (dwAttrib != INVALID_FILE_ATTRIBUTES &&
+            !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+}
+
+void print_help_message() {
+    DRINN_PRINT("\nUsage: drinn.exe [OPTIONS]\n\n");
+    DRINN_PRINT("        --help                      Print this message\n\n");
+    DRINN_PRINT("        --set-volume <volume>       Set volume.\n");
+    DRINN_PRINT("                                    Must be greater than 0 and less or equal to 1.\n");
+    DRINN_PRINT("                                    (default: 0.8)\n\n");
+    DRINN_PRINT("        --debug                     Activate debug output\n\n");
+    DRINN_PRINT("        --no-adapters-info          Do not print adapter info\n\n");
+    DRINN_PRINT("        --set-autostart             Set the program to autostart with the same flags\n\n");
 }
